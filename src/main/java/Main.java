91284d11ea2a203a992bcbd06b04cd7cb73df732
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-03-23 21:15:25 +0500

    [upd] Listening CMD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
 import App.DB;
 
+import static java.lang.Thread.sleep;
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-       String result = DB.search("Gadgedsadsdast");
-       if (result != null) {
-           System.out.println(result);
-       } else {
-           System.out.println("Product not found");
-       }
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello! Welcome to the Warhouse App");
+        System.out.println("We are making a connection to the database... please be patient");
+        Thread.sleep(2000);
+        System.out.println("Done! Connection established");
+        System.out.println("This app made by Abdullajonov Boburjon! Type \"/help\" to see all aviable comand");
+//      # Listen the console xD
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            if (command.equals("/help")) {
+                System.out.println("Wassup bro! Here is the list of all aviable commands");
+            }
+        }
     }
-
 }
