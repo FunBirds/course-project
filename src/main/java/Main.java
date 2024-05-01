2ee75f48a69788b992e4b852952b172ea956e2fe
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-05-01 21:42:45 +0500

    [upd]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import Dao.DaoFactory;
+import Dao.TablewareDao;
+import Dao.searchcriteria.Cup.CupSearchCriteria;
+import Dao.searchcriteria.SearchCriteria;
+import Dao.searchcriteria.parameter.CupParameters.ColorParameter;
+import EntityLayer.Cup;
+import Exceptions.DaoException;
+
+import java.util.Objects;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world, How's your day going?");
+    public static void main(String[] args) throws DaoException {
+        TablewareDao<Cup> dao = DaoFactory.INSTANCE.getTablewareDAO(Cup.class);
+        CupSearchCriteria criteria = new CupSearchCriteria();
+        criteria.addParameter(new ColorParameter("Red"));
+        dao.find(criteria).forEach(System.out::println);
     }
 }
