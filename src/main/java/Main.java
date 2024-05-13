724f35856fbba5598e8e839d89c1c044bc333ef7
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-05-13 11:14:26 +0500

    [upd][add] DotENV dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import ui.AppViewImpl;
+
 public class Main {
-    public static void main(String[] args)  {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        AppViewImpl appView = new AppViewImpl();
+        appView.launchAnimation();
     }
 }
