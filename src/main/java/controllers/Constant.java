a2173e00d4afdb34267b3da4593e323a301ced65
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-05-14 18:02:16 +0500

    [upd] Controller

---
+++ src/main/java/controllers/Constant.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+public abstract class Constant {
+    private final List<String> tablewares = List.of("Cup", "Plate", "Pot", "Spoon");
+    private final List<String> cupParameters = List.of("color", "size");
+    private final List<String> plateParameters = List.of("color", "shape");
+    private final List<String> potParameters = List.of("type");
+    private final List<String> spoonParameters = List.of("type");
+    private final List<String> generalParameter = List.of("material", "name");
+    private final List<String> AllParameters = List.of("color", "size", "shape", "type", "material", "name");
+}
