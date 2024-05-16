38742d6803f8df425be00ba5f36fb36c4cce4c6b
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-05-16 16:05:30 +0500

    [done][testing]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 import ui.UI;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
             UI ui = new UI();
             ui.start();
-        }catch(RuntimeException e){
-            throw new RuntimeException(e);
+        } catch(RuntimeException e){
+            System.err.println("An error occurred: " + e.getMessage());
+            System.out.println("Something went wrong... Please restart the program.");
         }
     }
 }
